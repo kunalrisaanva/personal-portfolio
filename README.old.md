07762d5281b18e876db0757d7c6b3453d76b961e
kunal <kunalrisaanva12@gmail.com>
2024-07-12 16:20:49 +0530

    basic structure ready

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# personal-portfoliio-
+personal-portfollio 
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <p>Portfollio</p>
+    </>
+  );
+}
+
+export default App;
